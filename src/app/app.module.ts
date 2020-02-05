bae32cdb8ee896765b8ade9fa131f155f7ef1cbc
Masbee29-code <masonhillsoftware@gmail.com>
2020-02-05 07:01:42 -0700

    Created base project, imported boottrap 4, created login-form and post-login components. Created usres list for testing login.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { PostLoginComponent } from './post-login/post-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent,
+    PostLoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/assets/users.ts
@@ -0,0 +1,5 @@
+const users = [
+    {username: "user1", password: "password"},
+    {username: "user2", password: "password"},
+    {username: "user3", password: "password"},
+];
